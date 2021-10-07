ac8aa7b7673df96ae0741be4c4b24de9e301a86d
Eliud <oeliud8@gmail.com>
2021-10-06 23:37:18 -0500

    updating database

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,19 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
+const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-	appId: process.env.REACT_APP_FIREBASE_APPID
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+	appId: process.env.REACT_APP_FIREBASE_APPID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  };
+  
+  // Initialize Firebase
+  firebase.default.initializeApp(firebaseConfig); 
 
 const db = firebase.firestore();
 const auth = firebase.auth();
